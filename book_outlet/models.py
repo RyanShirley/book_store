1c42f253481d214fe89a828d1864be0990439cba
Ryan <ryan.shirley@gmail.com>
2024-05-03 07:04:19 -0600

    76 Saving to the database

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Models have a built-in method called save() that writes the data contained within to the database
+# You can instantiate your model and give it some data at the same time, then call save() on the instance
+# to write the data to the database.
+
+# When you want to list all the items in a table, you can use some static methods on your class that 
+# extends Model.  The objects.all() method returns a list of your objects.  So in our case, we get a 
+# list of Book if we call Book.objects.all().  Then you can access the individual items in the list 
+# the usual way. e.g. Book.objects.all()[0] is an instance of book and you can access its attributes
+# with Book.objects.all()[0].title for example.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)

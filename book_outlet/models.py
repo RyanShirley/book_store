c8091800af5774efb179b5b15efd6ce296dfe712
Ryan <ryan.shirley@gmail.com>
2024-05-26 08:37:56 -0600

    90 slugify and overwriting save()

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,8 @@
+from typing import Iterable
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 # Models have a built-in method called save() that writes the data contained within to the database
@@ -60,10 +62,15 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
     
+    def save(self, *args, **kwargs):
+    
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating}/5)"

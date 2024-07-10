3065718b654aed28029a588a3aa087fb8ed101ce
Ryan <ryan.shirley@gmail.com>
2024-07-10 14:36:47 -0600

    Added local hosts and upgraded python

--- book_store/settings.py
+++ book_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uvy8*q3+i5=^jug5$pc1z4#*quztf$jjmuh_uy&c&1w%lpjn7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = [".awsapprunner.com","localhost","127.0.0.1"]
 
 
 # Application definition
